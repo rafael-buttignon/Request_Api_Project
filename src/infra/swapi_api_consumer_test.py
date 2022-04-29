9613dd3dd66b509389854ed94f24e86c6062d777
Rafael Augusto <rafaaugustocontato@gmail.com>
2022-04-28 23:40:42 -0300

    test: Testing mock http requests

--- src/infra/swapi_api_consumer_test.py
+++ src/infra/swapi_api_consumer_test.py
@@ -1,7 +1,11 @@
 from .swapi_api_consumer import SwapiApiConsumer
 
-def test_get_starships():
+def test_get_starships(requests_mock):
     ''' Testing get_starships method '''
+
+    requests_mock.get('https://swapi.dev/api/starships/', 
+    status_code=200, 
+    json={'results': [{'name': 'Starship 1'}, {'name': 'Starship 2'}]})
 
     swapi_api_consumer = SwapiApiConsumer()
     response = swapi_api_consumer.get_starships(page=1)
